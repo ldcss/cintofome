a8b7ca9b74320cf29e609341beb5a41e8d59f14e
Victor Kammerer <vko@cin.ufpe.br>
2023-05-05 00:19:05 -0300

    commitando server

--- rdt.py
+++ rdt.py
@@ -1,8 +1,9 @@
-from socket import *
-from aux_functions import checksum
+import socket
 import time
+from aux_functions import extract_packet, send_packet, wait_for_ack, send_ack, packet_loss, Packet
+from constants import BUFFER_SIZE, TIMEOUT_LIMIT, UDP_IP, UDP_PORT, PACKET_LOSS_PROB
 
-class Rdt:
+class RDT:
     #projeto ja se inicia com valores pré-setados
     def __init__(self, isServer = 0, addressPort = ("127.0.0.1", 20001), bufferSize = 1024):
         self.sender_addr = 0
@@ -29,6 +30,17 @@ class Rdt:
             """ print("Sending to server") """
             self.UDPSocket.sendto(data, self.addressPort)
 
-    def shutDown(self):
-         """ print("Closing socket") """
-        self.UDPSocket.close()
+    def send_packet(self, data):
+        data = self.create_header(data.decode())
+        ack = False
+
+        #envia o pacote enquanto o bit ACK for falso
+        while not ack:
+            self.send(data)
+
+            try:
+                data, self.sender_addr = self.UDPSocket.recvfrom(self.bufferSize)
+            except socket.timeout:
+                print("Did not receive ACK. Sending again.")
+            else:
+                ack = self.rcv_ack(data)
--- server.py
+++ server.py
@@ -95,7 +95,109 @@ while True:
    data = req.decode('utf8')
 
    match data:
+      
       case '1' | 'cardápio':
         res = hour() + " CINtofome:\n" + showMenu() + hour() + " " + nome + ": "
+
       case '2' | 'pedir':
-         res = hour() + " CINtofome: Digite o primeiro item que gostaria (número) \n" + hour() +" " + nome +": "
+        res = hour() + " CINtofome: Digite o primeiro item que gostaria (número) \n" + hour() +" " + nome +": "  
+        RDTSocket.send_pkg(res.encode())
+        data = RDTSocket.receive()
+
+      case 'nao':
+        res = hour() + " CINtofome: É pra já! \n" + hour() +" " + nome + ": "
+        not_payment = False
+
+      case '3' | 'conta individual':
+        total,value = getOrdersByClient(table,nome)
+        res = f"CINtofome: Sua conta total é:\n{value}-------------\nValor: {str(value)}"
+        res += f"\n{hour()} {nome}: "
+
+      case '4'| 'conta da mesa':
+        total = str(orders[table]["total"])
+        res = f"CINtofome:\n"
+
+        for client in orders[table]:
+          if client != "total":
+            total,value = getOrdersByClient(table,client)
+            if value > 0:
+              res += f"\n{client}\n{total}-------------\nValor: {str(value)}\n-------------"
+
+          res += f"\nValor total da mesa: {total}" 
+          res += f"\n{hour()} {nome}: "
+
+      case '5' | 'pagar':
+        comanda = allOrders[table][nome]["comanda"]
+        fechada = allOrders[table]["total"]
+        valido = False
+        dif = 0
+
+        # Repassa valores para o cliente e aguarda pagamento
+        base = f"Sua conta foi {comanda} e a da mesa foi {fechada}. Digite o valor a ser pago. \n{hour()} {nome}: "
+        res = f"{hour()} CINtofome: {base}"
+        RDTSocket.send_pkg(res.encode())
+        data = RDTSocket.receive() 
+        data = data.decode()
+
+        while True:
+
+          match data:  
+            case 'nao':
+              res = f"{horario()} CINtofome: Operação de pagamento cancelada!"
+              break
+
+            case valido & 'sim':       
+              allOrders[table][nome]["comanda"] = 0.0
+              allOrders[table][nome]["pedidos"] = []
+              allOrders[table]["total"] -= comanda
+
+              if dif > 0: 
+                allOrders[table]["total"] -= dif
+          
+           
+                com_conta = [c for c in allOrders[table] if c != "total" and allOrders[table][c]["comanda"] > 0]
+                dif_ind = dif/len(com_conta)
+
+                for cliente in com_conta:
+                  allOrders[table][client]["comanda"] -= dif
+
+              res = f"{hour()} CINtofome: Conta paga, obrigado! \n" 
+              not_payment = True
+              break
+
+        if (float(data) > comanda) and float(data) <= fechada: # Se tiver inserido um valor válido 
+          dif = float(data) - comanda
+          res = f"{hour()} Cintofome: Você está pagando {dif} a mais que sua conta.\n{hour()} Cintofome: O valor excedente será distribuído.{new_line}{horario()} Cintofome: "
+          valido = True
+          RDTSocket.send_pkg(resp.encode())
+        elif (float(data) == comanda): # Pagamento exato
+          res = f"{hour()} Cintofome: "
+          valido = True
+        elif (float(data) > fechada): # Se tiver inserido um valor maior do que a conta da mesa
+          res = f"{hour()} Cintofome: Valor maior do que o esperado, no momento não aceitamos gorjetas. \n" + base
+        elif (float(data) < comanda): # Se tiver inserido um valor menor que a conta individual
+          res = f"{hour()} Cintofome: Pagamento menor que o devido, nao fazemos fiado. \n" + base
+            
+        if valido:
+          res += f"Deseja confirmar o pagamento? (digite sim para confirmar)\n{hour()} {nome}: "
+
+        RDTSocket.send_pkg(res.encode())
+        data = RDTSocket.receive()
+        data = data.decode()
+        
+        res += f"{hour()} {nome}: "
+
+      case ('6' | 'levantar') & ~pagamento:
+        res = hour() + " " + nome + ": Você ainda não pagou sua conta\n" + hour() + " " + nome + ": "
+
+      case '6' | 'levantar' and pagamento:
+        res = "ok"
+        del allOrders[table][nome]
+        RDTSocket.send_pkg(resp.encode())
+
+        RDTSocket.send_pkg(resp.encode())
+
+        data = hour() + " " + nome + ": Volte sempre ^^ \n"
+        RDTSocket.send_pkg(data.encode())
+
+        RDTSocket.close_connection()
