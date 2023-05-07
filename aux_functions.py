6283e77525d8f2c40205962ca484e7403ef34b4f
Pedro Fernandes Barreto Costa <fernandespedrobc@gmail.com>
2023-05-06 21:23:44 -0300

    some advances

--- aux_functions.py
+++ aux_functions.py
@@ -82,3 +82,66 @@ def wait_for_ack(sock, expected_ack):
 
 def packet_loss(probability):
     return random.random() < probability
+
+def chunk_divide(data, head_size):
+    data_bytes = data.encode('utf-8')
+
+    chunk_size = BUFFER_SIZE - head_size
+
+    chunks = [data_bytes[i:i+chunk_size] for i in range(0, len(data_bytes), chunk_size)]
+
+    return chunks
+
+def sock_receive(sock, expected_seq_num):
+    received_data = "".encode('utf-8')
+
+    while True:
+        try:
+           # Recebe um pacote 
+            data, addr = sock.recvfrom(BUFFER_SIZE)
+            packet = extract_packet(data)
+            # filename = packet.data
+            if packet.seq_n == expected_seq_num:
+                # Avalia o checksum dele
+                if packet.checksum ==  packet.real_checksum():
+                    print(f"Pacote recebido: {packet.seq_n}")
+                    send_ack(sock, packet.seq_n, addr)
+                    expected_seq_num = 1 - expected_seq_num
+                    received_data += packet.data.encode('utf-8')
+                    if len(packet.data) + packet.reading_size() < BUFFER_SIZE:
+                        break
+                else:
+                    print(f"Checksum incorreto: {packet.checksum}, esperado: {packet.real_checksum()}")
+                    send_ack(sock, 1 - expected_seq_num, addr)
+            else:
+                print(f"Pacote incorreto: {packet.seq_n}, enviando ACK anterior")
+                send_ack(sock, 1 - expected_seq_num, addr)
+        except socket.timeout:
+            print(
+                f"Tempo limite de {TIMEOUT_LIMIT} segundos atingido. Encerrando conexão...")
+            break
+
+    return data, expected_seq_num, addr 
+
+def sock_send(message, sock, seq_num, addr):
+
+    packet = Packet(seq_num, False, "")
+
+    chunks = chunk_divide(message, packet.reading_size()) 
+
+    for data in chunks:
+        while True:
+            # Envia o pedaço de arquivo para o cliente usando rdt3.0
+            packet = Packet(seq_num, False, data.decode('utf-8'))
+            # print(f"Enviando pacote {seq_num}")
+            send_packet(sock, packet, addr)
+            ack_received = wait_for_ack(sock, seq_num)
+            if ack_received:
+                seq_num = 1 - seq_num
+
+                packet = Packet(seq_num, False, "")
+                break
+            else:
+                print("Reenviando pacote...")
+    
+    return seq_num
